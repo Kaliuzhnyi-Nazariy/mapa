b0cedf3b479651379a3e7932ade94a995f60cc51
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-06 17:34:15 +0200

    changed db connection

--- backend/db/db.ts
+++ backend/db/db.ts
@@ -1,12 +1,21 @@
 import { Pool } from "pg";
 import "dotenv/config";
 
+// const db = new Pool({
+//   host: process.env.PG_HOST,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+//   port: Number(process.env.PG_PORT),
+// });
+
 const db = new Pool({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: Number(process.env.PG_PORT),
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false },
 });
 
 export default db;
