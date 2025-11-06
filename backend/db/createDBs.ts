785f104c95420e608f1204b9ee99eb4425f46ce4
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-06 18:42:08 +0200

    splitted on 3 commands

--- backend/db/createDBs.ts
+++ backend/db/createDBs.ts
@@ -2,22 +2,27 @@ import db from "./db";
 
 const initTablesDB = async () => {
   await db.query(`
-  BEGIN;
+  
   CREATE DOMAIN IF NOT EXISTS not_empty AS varchar(128)
   CHECK (LENGTH(TRIM(VALUE)) > 0);
+  `);
+
+  await db.query(`
   CREATE TABLE IF NOT EXISTS Users (
     id SERIAL PRIMARY KEY,
     name not_empty NOT NULL,
     email not_empty NOT NULL UNIQUE,
     password varchar(156)
   );
+  `);
+
+  await db.query(`
   CREATE TABLE IF NOT EXISTS Map_points (
     id SERIAL PRIMARY KEY,
     name varchar(255),
     position JSON NOT NULL,
     owner_id int NOT NULL REFERENCES Users(id) ON DELETE CASCADE
   );
-  COMMIT;
 `);
 };
 
