01f5aaced3610ea4633cf4ee92c61bf9a9cf46a0
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-06 18:11:23 +0200

    add condional creating domain

--- backend/db/createDBs.ts
+++ backend/db/createDBs.ts
@@ -2,7 +2,7 @@ import db from "./db";
 
 const initTablesDB = async () => {
   await db.query(`
-  CREATE DOMAIN not_empty AS varchar(128) CHECK (LENGTH(TRIM(VALUE)) > 0);
+  CREATE DOMAIN IF NOT EXISTS not_empty AS varchar(128) CHECK (LENGTH(TRIM(VALUE)) > 0);
 
   CREATE TABLE IF NOT EXISTS Users (
   id SERIAL NOT NULL,
