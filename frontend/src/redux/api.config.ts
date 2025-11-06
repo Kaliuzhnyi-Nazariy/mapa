bfc75baa1c412ecbebb1a403930e3ce8a15bd934
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-06 11:29:33 +0200

    add /api for deployed version

--- frontend/src/redux/api.config.ts
+++ frontend/src/redux/api.config.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.NODE_ENV === "production"
-      ? ""
+      ? "/api"
       : "http://localhost:3001/api",
   withCredentials: true,
 });
