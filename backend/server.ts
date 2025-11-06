0d16495f5f202d17f19edde2de2544a6a9ca61eb
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-06 11:12:11 +0200

    add middlewares and added routes

--- backend/server.ts
+++ backend/server.ts
@@ -1,38 +1,40 @@
-import express, {
-  request,
-  response,
-  NextFunction,
-  Response,
-  Request,
-} from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { ajMiddleware } from "./middlewares/aj";
+import cookieParser from "cookie-parser";
+import { ajMiddleware } from "./middlewares";
 import db from "./db/db";
 import initTablesDB from "./db/createDBs";
+import authRoutes from "./routes/auth";
+import errorRoute from "./routes/error";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  })
+);
+
+app.use(cookieParser());
 
 app.use(ajMiddleware);
 
-app.get("/", async (req: Request, res: Response) => {
-  const { rows } = await db.query("SELECT * FROM Users;");
-  res.send({ rows });
-});
+app.use("/api/auth", authRoutes);
+
+app.use(errorRoute);
 
 db.connect()
   .then(() => {
-    console.log("DB successfully up and running!");
     initTablesDB().then(() => {
       try {
-        app.listen(process.env.PORT, () => {
-          console.log("everything is running successfully!");
-        });
+        app.listen(process.env.PORT);
       } catch (error) {
         console.log(error);
         process.exit(1);
