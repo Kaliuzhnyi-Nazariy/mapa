05201b111ccfb28125efbd97da00b80a07e63187
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-06 11:11:36 +0200

    changed creating table adding custom module

--- backend/db/createDBs.ts
+++ backend/db/createDBs.ts
@@ -2,21 +2,22 @@ import db from "./db";
 
 const initTablesDB = async () => {
   await db.query(`
-    CREATE TABLE IF NOT EXISTS Users (
-    id SERIAL NOT NULL,
-    name varchar(128) NOT NULL,
-    email varchar(128) NOT NULL UNIQUE,
-    password varchar(156),
-    PRIMARY KEY (id)
-    );
+  CREATE TABLE IF NOT EXISTS Users (
+  id SERIAL NOT NULL,
+  name not_empty NOT NULL,
+  email not_empty NOT NULL UNIQUE,
+  password varchar(156),
+  PRIMARY KEY (id)
+  );
 
-    CREATE TABLE IF NOT EXISTS Map_points (
-    id SERIAL NOT NULL PRIMARY KEY,
-    name varchar(255),
-    position JSON NOT NULL,
-    owner_id int NOT NULL,
-    FOREIGN KEY (owner_id) REFERENCES Users(id) ON DELETE CASCADE
-    );
+  CREATE TABLE IF NOT EXISTS Map_points (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name varchar(255),
+  position JSON NOT NULL,
+  owner_id int NOT NULL,
+  FOREIGN KEY (owner_id) REFERENCES Users(id) ON DELETE CASCADE
+  );
+
 `);
 };
 
