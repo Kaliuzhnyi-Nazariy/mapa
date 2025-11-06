7bbfbf18dd08745d44e0330c972a498cda96bc19
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-06 18:35:47 +0200

    changed command

--- backend/db/createDBs.ts
+++ backend/db/createDBs.ts
@@ -2,24 +2,22 @@ import db from "./db";
 
 const initTablesDB = async () => {
   await db.query(`
-  CREATE DOMAIN IF NOT EXISTS not_empty AS varchar(128) CHECK (LENGTH(TRIM(VALUE)) > 0);
-
+  BEGIN;
+  CREATE DOMAIN IF NOT EXISTS not_empty AS varchar(128)
+  CHECK (LENGTH(TRIM(VALUE)) > 0);
   CREATE TABLE IF NOT EXISTS Users (
-  id SERIAL NOT NULL,
-  name not_empty NOT NULL,
-  email not_empty NOT NULL UNIQUE,
-  password varchar(156),
-  PRIMARY KEY (id)
+    id SERIAL PRIMARY KEY,
+    name not_empty NOT NULL,
+    email not_empty NOT NULL UNIQUE,
+    password varchar(156)
   );
-
   CREATE TABLE IF NOT EXISTS Map_points (
-  id SERIAL NOT NULL PRIMARY KEY,
-  name varchar(255),
-  position JSON NOT NULL,
-  owner_id int NOT NULL,
-  FOREIGN KEY (owner_id) REFERENCES Users(id) ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    name varchar(255),
+    position JSON NOT NULL,
+    owner_id int NOT NULL REFERENCES Users(id) ON DELETE CASCADE
   );
-
+  COMMIT;
 `);
 };
 
