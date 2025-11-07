2880b3a004e44e87efdebd6da0a6a035468753b6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-07 17:51:55 +0200

    change link

--- backend/server.ts
+++ backend/server.ts
@@ -32,7 +32,7 @@ app.use("/api/auth", authRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.all("/{*any}", (_req, res) => {
+  app.all("*", (_req, res) => {
     res.setHeader(
       "Content-Security-Policy",
       [
