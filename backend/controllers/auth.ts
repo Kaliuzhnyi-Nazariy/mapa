4f12700ff3eb4add4dc50c77ad1bf7ee1d72693f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-25 09:43:55 +0100

    auth controller

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { ctrlWrapper, errorHandler } from "../helpers";
 import db from "../db/db";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { UserRequest } from "../middlewares/authenticated";
 
 export interface SignUp {
   name: string;
@@ -18,9 +20,23 @@ const signup = async (req: Request, res: Response, next: NextFunction) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const { rows } = await db.query(
-    "INSERT INTO Users (name, email, password) VALUES ($1, $2, $3) RETURNING *;",
+    "INSERT INTO Users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email;",
     [name, email, hashedPassword]
   );
+
+  const payload = {
+    id: rows[0].id,
+  };
+
+  const token = jwt.sign(payload, process.env.SECRET_JWT!);
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
+    maxAge: 86400000,
+  });
 
   res.status(201).json(rows[0]);
 };
@@ -28,8 +44,11 @@ const signup = async (req: Request, res: Response, next: NextFunction) => {
 const signin = async (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
-  if (!email || !password)
-    next(errorHandler(400, "All fields should be filled!"));
+  if (!email || !password) {
+    return next(errorHandler(400, "All fields should be filled!"));
+  }
+
+  // console.log(email, password);
 
   const { rows } = await db.query("SELECT * FROM Users where email=$1", [
     email,
@@ -45,18 +64,31 @@ const signin = async (req: Request, res: Response, next: NextFunction) => {
     return next(errorHandler(400, "Email or password is not matching!"));
   }
 
-  res.cookie("token", "token", {
+  const payload = {
+    id: rows[0].id,
+  };
+
+  const token = jwt.sign(payload, process.env.SECRET_JWT!);
+
+  res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     path: "/",
-    maxAge: 86400,
+    maxAge: 86400000,
   });
 
   res.status(200).json({ name: rows[0].name, email: rows[0].email });
 };
 
 const signout = async (req: Request, res: Response, next: NextFunction) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
+    maxAge: 86400,
+  });
   res.status(204).json();
 };
 
