85eb69090871de9af9032fc812e42f2803942cd2
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-25 10:11:36 +0100

    main app component + routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,38 @@
-import Signup from "./components/auth/signup";
-
+import "mapbox-gl/dist/mapbox-gl.css";
+import MapComponent from "./components/Map/MapComponent";
+import AuthPage from "./components/auth/AuthPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RestrictedRoute from "./Routes/RestrictedRoute";
+import { useAppDispatch } from "./redux/dispatch";
+import { useEffect } from "react";
+import { getMe } from "./redux/user/userRequests";
+import PrivateRoute from "./Routes/PrivateRoute";
+import GreetingPage from "./components/greeting/GreetingPage";
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, []);
+
   return (
-    <>
-      <Signup />
-    </>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<GreetingPage />} />
+        <Route
+          path="/auth"
+          element={
+            <RestrictedRoute component={<AuthPage />} redirectTo="/map" />
+          }
+        />
+        <Route
+          path="/map"
+          element={
+            <PrivateRoute component={<MapComponent />} redirectTo="/auth" />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
