dd10a24d8e6981e6e14de354f3e1a1012a9f94c2
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-25 09:47:27 +0100

    create auth middleware

--- backend/middlewares/authenticated.ts
+++ backend/middlewares/authenticated.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { errorHandler } from "../helpers";
+import db from "../db/db";
+import { decode, JwtPayload } from "jsonwebtoken";
 
 export interface UserRequest extends Request {
-  // user: {
-  //     name: string,
-  //     user: string
-  // }
+  user: {
+    id: string;
+    name: string;
+    email: string;
+  };
   token: string;
 }
 
@@ -14,13 +17,29 @@ export const isAuthenticated = async (
   res: Response,
   next: NextFunction
 ) => {
-  console.log(req.headers["set-cookie"]);
-  console.log(req.cookies);
+  // console.log("headers: ", req.headers["set-cookie"]);
+  // console.log("cookies: ", req.cookies);
+  // console.log("cookies token: ", req.cookies.token);
 
   const tokenCookie = req.cookies.token;
   if (!tokenCookie) {
-    next(errorHandler(401, "No token!"));
+    return next(errorHandler(401, "No token!"));
   }
+
+  const { id } = decode(tokenCookie) as { id: string | JwtPayload };
+
+  // console.log({ id });
+
+  const { rows } = await db.query(
+    "SELECT id, name, email FROM Users WHERE id =$1",
+    [id]
+  );
+
+  if (rows.length == 0) {
+    next(errorHandler(401));
+  }
+
+  (req as unknown as UserRequest).user = rows[0];
 
   next();
 };
