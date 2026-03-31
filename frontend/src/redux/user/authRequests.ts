4493e2ac0e9e824528eec78379d62f561e20f30c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-31 18:19:04 +0200

    fix: remove redux operations for auth

--- frontend/src/redux/user/authRequests.ts
+++ frontend/src/redux/user/authRequests.ts
@@ -1,61 +1,63 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import api from "../api.config";
-import axios from "axios";
-import type { ReturnUser, SignInUser, SignUpUser } from "./userTypes";
+// import { createAsyncThunk } from "@reduxjs/toolkit";
+// import api from "../api.config";
+// import axios from "axios";
+// import type { ReturnUser, SignInUser, SignUpUser } from "./userTypes";
 
-export const signup = createAsyncThunk<
-  ReturnUser,
-  SignUpUser,
-  { rejectValue: { message: string } }
->("auth/signup", async (data, { rejectWithValue }) => {
-  try {
-    const res = await api.post("/auth/signup", data);
-    return res.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      return rejectWithValue({
-        message: error.response?.data?.message || "Signin failed",
-      });
-    }
-    return rejectWithValue({ message: "Unexpected error occurred" });
-  }
-});
+// export const signup = createAsyncThunk<
+//   ReturnUser,
+//   SignUpUser,
+//   { rejectValue: { message: string } }
+// >("auth/signup", async (data, { rejectWithValue }) => {
+//   try {
+//     const res = await api.post("/auth/signup", data);
+//     return res.data;
+//   } catch (error) {
+//     if (axios.isAxiosError(error)) {
+//       return rejectWithValue({
+//         message: error.response?.data?.message || "Signin failed",
+//       });
+//     }
+//     return rejectWithValue({ message: "Unexpected error occurred" });
+//   }
+// });
 
-export const signin = createAsyncThunk<
-  ReturnUser,
-  SignInUser,
-  { rejectValue: { message: string } }
->("auth/signin", async (data, { rejectWithValue }) => {
-  try {
-    // console.log({ data });
-    const res = await api.post("/auth/signin", data, { withCredentials: true });
-    return res.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      // console.log(error);
-      return rejectWithValue({
-        message: error.response?.data?.message || "Signin failed",
-      });
-    }
-    return rejectWithValue({ message: "Unexpected error occurred" });
-  }
-});
+// export const signin = createAsyncThunk<
+//   ReturnUser,
+//   SignInUser,
+//   { rejectValue: { message: string } }
+// >("auth/signin", async (data, { rejectWithValue }) => {
+//   try {
+//     // console.log({ data });
+//     const res = await api.post("/auth/signin", data, { withCredentials: true });
+//     return res.data;
+//   } catch (error) {
+//     // console.log(error);
+//     if (axios.isAxiosError(error)) {
+//       // console.log(error);
+//       throw rejectWithValue({
+//         message: error.response?.data?.message || "Signin failed",
+//       });
+//     } else {
+//       throw rejectWithValue({ message: "Unexpected error occurred" });
+//     }
+//   }
+// });
 
-export const signout = createAsyncThunk<
-  void,
-  void,
-  { rejectValue: { message: string } }
->("auth/signout", async (_, { rejectWithValue }) => {
-  try {
-    const res = await api.post("/auth/signout", { withCredentials: true });
-    return res.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      // console.log(error);
-      return rejectWithValue({
-        message: error.response?.data?.message || "Signin failed",
-      });
-    }
-    return rejectWithValue({ message: "Unexpected error occurred" });
-  }
-});
+// export const signout = createAsyncThunk<
+//   void,
+//   void,
+//   { rejectValue: { message: string } }
+// >("auth/signout", async (_, { rejectWithValue }) => {
+//   try {
+//     const res = await api.post("/auth/signout", { withCredentials: true });
+//     return res.data;
+//   } catch (error) {
+//     if (axios.isAxiosError(error)) {
+//       // console.log(error);
+//       return rejectWithValue({
+//         message: error.response?.data?.message || "Signin failed",
+//       });
+//     }
+//     return rejectWithValue({ message: "Unexpected error occurred" });
+//   }
+// });
--- frontend/src/redux/user/slice.ts
+++ frontend/src/redux/user/slice.ts
@@ -1,5 +1,5 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
-import { signin, signout, signup } from "./authRequests";
+// import { signin, signout, signup } from "./authRequests";
 import { getMe } from "./userRequests";
 
 interface UserInitialState {
@@ -24,18 +24,18 @@ const initialState: UserInitialState = {
   isRefreshing: false,
 };
 
-const pendingHandler = (state: UserInitialState) => {
-  state.error = null;
-  state.isLoading = true;
-};
+// const pendingHandler = (state: UserInitialState) => {
+//   state.error = null;
+//   state.isLoading = true;
+// };
 
-const rejectionHandler = (
-  state: UserInitialState,
-  action: PayloadAction<{ message: string } | undefined>
-) => {
-  state.isLoading = false;
-  state.error = action.payload?.message ?? "Unexpected error occurred";
-};
+// const rejectionHandler = (
+//   state: UserInitialState,
+//   action: PayloadAction<{ message: string } | undefined>,
+// ) => {
+//   state.isLoading = false;
+//   state.error = action.payload?.message ?? "Unexpected error occurred";
+// };
 
 const userSlice = createSlice({
   name: "user",
@@ -43,41 +43,41 @@ const userSlice = createSlice({
   reducers: {},
   extraReducers(builder) {
     builder
-      .addCase(signup.pending, pendingHandler)
-      .addCase(
-        signup.fulfilled,
-        (
-          state: UserInitialState,
-          action: PayloadAction<{ name: string; email: string }>
-        ) => {
-          state.isLoading = false;
-          state.isLoggedIn = true;
-          state.user = action.payload;
-        }
-      )
-      .addCase(signup.rejected, rejectionHandler)
+      // .addCase(signup.pending, pendingHandler)
+      // .addCase(
+      //   signup.fulfilled,
+      //   (
+      //     state: UserInitialState,
+      //     action: PayloadAction<{ name: string; email: string }>
+      //   ) => {
+      //     state.isLoading = false;
+      //     state.isLoggedIn = true;
+      //     state.user = action.payload;
+      //   }
+      // )
+      // .addCase(signup.rejected, rejectionHandler)
 
-      .addCase(signin.pending, pendingHandler)
-      .addCase(
-        signin.fulfilled,
-        (
-          state: UserInitialState,
-          action: PayloadAction<{ name: string; email: string }>
-        ) => {
-          state.isLoading = false;
-          state.isLoggedIn = true;
-          state.user = action.payload;
-        }
-      )
-      .addCase(signin.rejected, rejectionHandler)
+      // .addCase(signin.pending, pendingHandler)
+      // .addCase(
+      //   signin.fulfilled,
+      //   (
+      //     state: UserInitialState,
+      //     action: PayloadAction<{ name: string; email: string }>
+      //   ) => {
+      //     state.isLoading = false;
+      //     state.isLoggedIn = true;
+      //     state.user = action.payload;
+      //   }
+      // )
+      // .addCase(signin.rejected, rejectionHandler)
 
-      .addCase(signout.pending, pendingHandler)
-      .addCase(signout.fulfilled, (state: UserInitialState) => {
-        state.user = initialState.user;
-        state.isLoading = initialState.isLoading;
-        state.isLoggedIn = initialState.isLoggedIn;
-      })
-      .addCase(signout.rejected, rejectionHandler)
+      // .addCase(signout.pending, pendingHandler)
+      // .addCase(signout.fulfilled, (state: UserInitialState) => {
+      //   state.user = initialState.user;
+      //   state.isLoading = initialState.isLoading;
+      //   state.isLoggedIn = initialState.isLoggedIn;
+      // })
+      // .addCase(signout.rejected, rejectionHandler)
 
       .addCase(getMe.pending, (state: UserInitialState) => {
         state.isRefreshing = true;
@@ -86,23 +86,23 @@ const userSlice = createSlice({
         getMe.fulfilled,
         (
           state: UserInitialState,
-          action: PayloadAction<{ name: string; email: string }>
+          action: PayloadAction<{ name: string; email: string }>,
         ) => {
           // console.log(action.payload);
           state.user = action.payload;
           state.isRefreshing = false;
           state.isLoggedIn = true;
-        }
+        },
       )
       .addCase(
         getMe.rejected,
         (
           state: UserInitialState,
-          action: PayloadAction<{ message: string } | undefined>
+          action: PayloadAction<{ message: string } | undefined>,
         ) => {
           state.isRefreshing = false;
           state.error = action.payload?.message ?? "Unexpected error occurred";
-        }
+        },
       );
   },
 });
