7f41fc72000a08642cdbba3ce9e0b266a3f37d93
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-04-02 10:49:33 +0200

    fix: split responsobilities

--- frontend/src/components/Map/MapComponent.tsx
+++ frontend/src/components/Map/MapComponent.tsx
@@ -1,19 +1,203 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch } from "../../redux/dispatch";
 import { getMarkers } from "../../redux/marker/request";
 import { MapContainer } from "./MapContainer";
+import SideMenu from "../Menu/SideMenu";
+import { useInitMap } from "./initMap";
+import AddMarkerModal from "./AddMarkerModal";
+import { useSelector } from "react-redux";
+import { markers } from "../../redux/marker/selector";
+import { generateNewMarker, removeMarkers } from "./useMap";
+import EditMarkerModal from "./EditMarkerModal";
+import Menu from "../Menu/Menu";
+
+export interface Coordinates {
+  lng: number;
+  lat: number;
+}
+
+export interface Markers {
+  id: string;
+  name: string;
+  position: { lng: number; lat: number };
+}
 
 const MapComponent = () => {
   const dispatch = useAppDispatch();
+
+  const markersList = useSelector(markers);
 
   useEffect(() => {
     dispatch(getMarkers());
   }, []);
 
+  const {
+    mapContainerRef,
+    mapRef,
+    // initialCoords,
+    // setInitialCoords,
+    // clickHandle,
+    setOnMapClick,
+  } = useInitMap();
+
+  const [lngLat, setLngLat] = useState<Coordinates | null>(null);
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const openMenu = () => setMenuOpen(true);
+
+  const resetMenu = () => setMenuOpen(false);
+
+  const [chosenMarker, setMarker] = useState<number | null>(null);
+
+  useEffect(() => {
+    setOnMapClick((e: mapboxgl.MapMouseEvent) => {
+      const target = e.originalEvent.target as HTMLElement | null;
+
+      const isClickData = target
+        ?.closest("div")
+        ?.classList.contains("mapboxgl-marker");
+
+      if (isClickData) {
+        const clickData = target?.closest("div");
+
+        if (!clickData) {
+          // setMarker();
+          return;
+        }
+
+        const id = clickData.getAttribute("marker_id");
+
+        const typeOfMarker = clickData.getAttribute("marker_type");
+
+        if (typeOfMarker == "found") {
+          return;
+        }
+
+        openMenu();
+
+        if (!chosenMarker) {
+          setMarker(Number(id));
+        }
+
+        return;
+      }
+
+      setLngLat({
+        lat: e.lngLat.lat,
+        lng: e.lngLat.lng,
+      });
+
+      openModal();
+    });
+  }, [setOnMapClick]);
+
+  useEffect(() => {
+    if (!mapRef.current) return;
+
+    if (markersList.length === 0) return;
+
+    markersList.map((m) => {
+      removeMarkers({
+        map: mapRef,
+        markerId: m.id,
+        type: "users",
+      });
+    });
+
+    markersList.map((m) => {
+      generateNewMarker({
+        lng: m.position.lng,
+        lat: m.position.lat,
+        map: mapRef.current!,
+        type: "users",
+        name: m.name,
+        id: m.id,
+        openEdit,
+      });
+    });
+  }, [markersList]);
+
+  const [isEditOpen, setEditOpen] = useState(false);
+
+  const [editName, setEditName] = useState("");
+  const [editId, setEditId] = useState("");
+  const [editLat, setEditLat] = useState<number | null>(null);
+  const [editLng, setEditLng] = useState<number | null>(null);
+
+  const closeEdit = () => {
+    setEditOpen(false);
+  };
+
+  const openEdit = ({
+    name,
+    id,
+    lng,
+    lat,
+  }: {
+    name: string;
+    id: string;
+    lng: number;
+    lat: number;
+  }) => {
+    setEditOpen(true);
+    setEditName(name);
+    setEditId(id);
+    setEditLat(lat);
+    setEditLng(lng);
+  };
+
+  setTimeout(() => {
+    setMarker(null);
+  }, 500);
+
   return (
-    <>
-      <MapContainer />
-    </>
+    <div className="min-[1440px]:grid min-[1440px]:grid-cols-[25vw_75vw] min-[1440px]:grid-rows-[88px_auto] min-[1440px]:overflow-hidden w-full min-h-screen overflow-hidden h-screen">
+      <Menu
+        mapRef={mapRef}
+        menuTrigger={isMenuOpen}
+        id={chosenMarker}
+        resetMenu={resetMenu}
+        // restMarker={restMarker}
+        lngLat={lngLat}
+        openEdit={openEdit}
+        extraStyles="col-start-1 col-end-3 row-start-1"
+      />
+      <SideMenu
+        mapRef={mapRef}
+        id={chosenMarker}
+        openEdit={openEdit}
+        // extraStyles="col-start-1 col-end-3 row-start-1"
+      />
+      <MapContainer mapContainerRef={mapContainerRef} />
+      <AddMarkerModal
+        isShown={isModalOpen}
+        closeModal={closeModal}
+        lngLat={lngLat}
+        mapRef={mapRef}
+        openEdit={openEdit}
+      />{" "}
+      <EditMarkerModal
+        isModalOpen={isEditOpen}
+        closeModal={closeEdit}
+        name={editName}
+        id={editId}
+        lat={editLat}
+        lng={editLng}
+        map={mapRef}
+        openEdit={openEdit}
+      />{" "}
+    </div>
   );
 };
 
--- frontend/src/components/Map/MapContainer.tsx
+++ frontend/src/components/Map/MapContainer.tsx
@@ -1,202 +1,85 @@
-import { useEffect, useRef, useState } from "react";
+// import { useEffect, useRef, useState } from "react";
 
 import "mapbox-gl/dist/mapbox-gl.css";
-import { generateNewMarker } from "./useMap";
+// import { generateNewMarker } from "./useMap";
 import { Map } from "mapbox-gl";
-import { useSelector } from "react-redux";
-import { markers } from "../../redux/marker/selector";
-import Locations from "./Locations";
-import AddMarkerModal from "./AddMarkerModal";
-import EditMarkerModal from "./EditMarkerModal";
-import { useInitMap } from "./initMap";
-import Menu from "../Menu/Menu";
-import SideMenu from "../Menu/SideMenu";
+import {
+  // useEffect,
+  type Ref,
+} from "react";
+// import { useSelector } from "react-redux";
+// import { markers } from "../../redux/marker/selector";
+// import Locations from "./Locations";
+// import AddMarkerModal from "./AddMarkerModal";
+// import EditMarkerModal from "./EditMarkerModal";
+// import { useInitMap } from "./initMap";
+// import Menu from "../Menu/Menu";
+// import SideMenu from "../Menu/SideMenu";
 
-export const MapContainer = () => {
-  const { mapContainerRef, mapRef, initialCoords, setInitialCoords } =
-    useInitMap();
+export const MapContainer = ({
+  mapContainerRef,
+}: // clickHandle,
+// mapRef,
+{
+  mapContainerRef: Ref<HTMLDivElement>;
+  // clickHandle: (e: mapboxgl.MapMouseEvent | unknown) => void;
+  // mapRef: Ref<mapboxgl.Map | null>;
+}) => {
+  // useEffect(() => {
+  //   if (!mapRef || !mapRef.current) return;
 
-  const lngLat = useRef<{ lng: number; lat: number } | null>(null);
+  //   const handleClick = (e: mapboxgl.MapMouseEvent) => {
+  //     const target = e.originalEvent.target as HTMLElement | null;
 
-  const [markersList, setMarkers] = useState<
-    {
-      id: string;
-      name: string;
-      position: { lng: number; lat: number };
-      owner_id?: string;
-    }[]
-  >([]);
+  //     const isClickData = target
+  //       ?.closest("div")
+  //       ?.classList.contains("mapboxgl-marker");
 
-  const userMarkers = useSelector(markers);
+  //     if (isClickData) {
+  //       const clickData = target?.closest("div");
+  //       // console.log(clickData);
+  //       // console.log("clicking id: ", clickData.getAttribute("marker_id"));
+  //       if (!clickData) return;
 
-  useEffect(() => {
-    setMarkers(userMarkers);
-  }, [userMarkers]);
+  //       const id = clickData.getAttribute("marker_id");
 
-  useEffect(() => {
-    if (!navigator.geolocation) return;
+  //       const typeOfMarker = clickData.getAttribute("marker_type");
 
-    navigator.geolocation.getCurrentPosition((pos) => {
-      const { latitude, longitude } = pos.coords;
+  //       if (typeOfMarker == "found") {
+  //         return;
+  //       }
 
-      setInitialCoords([longitude, latitude]);
-    });
-  }, []);
+  //       console.log(id);
 
-  useEffect(() => {
-    if (!mapRef.current) return;
+  //       // openMenu();
+  //       // setMarker(Number(id));
 
-    mapRef.current.setCenter(initialCoords);
-  }, [initialCoords]);
+  //       return;
+  //     }
 
-  const [isModalOpen, setModalOpen] = useState(false);
+  //     console.log("modal is open");
+  //     // lngLat.current = {
+  //     //   lat: e.lngLat.lat,
+  //     //   lng: e.lngLat.lng,
+  //     // };
 
-  const openModal = () => {
-    setModalOpen(true);
-  };
+  //     // openModal();
+  //   };
 
-  const closeModal = () => {
-    setModalOpen(false);
-  };
+  //   mapRef.current?.on("click", handleClick);
 
-  const [isMunuOpen, setMenuOpen] = useState(false);
-
-  const openMenu = () => setMenuOpen(true);
-
-  const resetMenu = () => setMenuOpen(false);
-
-  const [chosenMarker, setMarker] = useState<number | null>(null);
-
-  const restMarker = () => setMarker(null);
-
-  useEffect(() => {
-    if (!mapRef.current) return;
-
-    const handleClick = (e: mapboxgl.MapMouseEvent) => {
-      const target = e.originalEvent.target as HTMLElement | null;
-
-      const isClickData = target
-        ?.closest("div")
-        ?.classList.contains("mapboxgl-marker");
-
-      if (isClickData) {
-        const clickData = target?.closest("div");
-        // console.log(clickData);
-        // console.log("clicking id: ", clickData.getAttribute("marker_id"));
-        if (!clickData) return;
-
-        const id = clickData.getAttribute("marker_id");
-
-        const typeOfMarker = clickData.getAttribute("marker_type");
-
-        if (typeOfMarker == "found") {
-          return;
-        }
-
-        openMenu();
-        setMarker(Number(id));
-
-        return;
-      }
-
-      lngLat.current = {
-        lat: e.lngLat.lat,
-        lng: e.lngLat.lng,
-      };
-
-      openModal();
-    };
-
-    mapRef.current?.on("click", handleClick);
-
-    return () => {
-      mapRef.current?.off("click", handleClick);
-    };
-  }, []);
-
-  const [isEditOpen, setEditOpen] = useState(false);
-
-  const [editName, setEditName] = useState("");
-  const [editId, setEditId] = useState("");
-  const [editLat, setEditLat] = useState<number | null>(null);
-  const [editLng, setEditLng] = useState<number | null>(null);
-
-  const closeEdit = () => {
-    setEditOpen(false);
-  };
-
-  const openEdit = ({
-    name,
-    id,
-    lng,
-    lat,
-  }: {
-    name: string;
-    id: string;
-    lng: number;
-    lat: number;
-  }) => {
-    setEditOpen(true);
-    setEditName(name);
-    setEditId(id);
-    setEditLat(lat);
-    setEditLng(lng);
-  };
-
-  useEffect(() => {
-    if (!mapRef.current) return;
-
-    const map = mapRef.current;
-
-    const handleMapLoad = () => {
-      markersList.forEach((m) => {
-        return generateNewMarker({
-          map,
-          ...{
-            lng: m.position.lng,
-            lat: m.position.lat,
-            name: m.name,
-            id: m.id,
-            openEdit: openEdit,
-            closeEdit: closeEdit,
-          },
-          type: "users",
-        });
-      });
-    };
-
-    map.on("load", handleMapLoad);
-
-    return () => {
-      map.off("load", handleMapLoad);
-    };
-  }, [markersList]);
+  //   return () => {
+  //     mapRef.current?.off("click", handleClick);
+  //   };
+  // }, []);
 
   return (
-    <div className="min-[1440px]:grid min-[1440px]:grid-cols-[25vw_75vw] min-[1440px]:grid-rows-[88px_auto] min-[1440px]:overflow-hidden w-full min-h-screen">
-      <Menu
-        mapRef={mapRef}
-        menuTrigger={isMunuOpen}
-        id={chosenMarker}
-        resetMenu={resetMenu}
-        restMarker={restMarker}
-        lngLat={lngLat}
-        openEdit={openEdit}
-        extraStyles="col-start-1 col-end-3 row-start-1"
-      />
-
-      <SideMenu
-        mapRef={mapRef}
-        id={chosenMarker}
-        openEdit={openEdit}
-        // extraStyles="col-start-1 col-end-3 row-start-1"
-      />
-
+    <>
       <div
         id="map-container"
         className="
       relative 
-      w-[100vw] h-[100vh]
+      w-screen h-screen
       min-[1440px]:w-[75vw] 
       min-[1440px]:h-[calc(100vh-88px)]
       min-[1440px]:col-start-2 
@@ -204,10 +87,10 @@ export const MapContainer = () => {
     "
         ref={mapContainerRef}
       />
-      <div className="fixed top-0 left-0 w-full min-[768px]:hidden ">
-        <Locations mapRef={mapRef} lngLat={lngLat} markersList={markersList} />
-      </div>
-      <AddMarkerModal
+      {/* <div className="fixed top-0 left-0 w-full min-[768px]:hidden ">
+        <Locations mapRef={mapRef} lngLat={lngLat} markersList={userMarkers} />
+      </div> */}
+      {/*<AddMarkerModal
         isShown={isModalOpen}
         closeModal={closeModal}
         lngLat={lngLat.current}
@@ -223,8 +106,8 @@ export const MapContainer = () => {
         lng={editLng}
         map={mapRef}
         openEdit={openEdit}
-      />
-    </div>
+      /> */}
+    </>
   );
 };
 
