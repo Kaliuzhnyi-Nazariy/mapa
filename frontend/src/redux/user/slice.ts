c1fdf94b8b8322b033a7ddeab00892af4239fc1c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-09-22 23:15:18 +0200

    feat: add redux-persist

--- frontend/src/redux/user/slice.ts
+++ frontend/src/redux/user/slice.ts
@@ -1,18 +1,28 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
-// import { signin, signout, signup } from "./authRequests";
-import { getMe } from "./userRequests";
+// import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+// // import { signin, signout, signup } from "./authRequests";
+// import { getMe } from "./userRequests";
 
-interface UserInitialState {
-  user: {
-    name: string | null;
-    email: string | null;
-  };
-  isLoading: boolean;
-  isLoggedIn: boolean;
-  error: string | null;
-  isRefreshing: boolean;
-}
+// interface UserInitialState {
+//   user: {
+//     name: string | null;
+//     email: string | null;
+//   };
+//   isLoading: boolean;
+//   isLoggedIn: boolean;
+//   error: string | null;
+//   isRefreshing: boolean;
+// }
 
+// // const initialState: UserInitialState = {
+// //   user: {
+// //     name: null,
+// //     email: null,
+// //   },
+// //   isLoading: false,
+// //   isLoggedIn: false,
+// //   error: null,
+// //   isRefreshing: false,
+// // };
 // const initialState: UserInitialState = {
 //   user: {
 //     name: null,
@@ -21,31 +31,128 @@ interface UserInitialState {
 //   isLoading: false,
 //   isLoggedIn: false,
 //   error: null,
-//   isRefreshing: false,
+//   isRefreshing: true,
 // };
+
+// // const pendingHandler = (state: UserInitialState) => {
+// //   state.error = null;
+// //   state.isLoading = true;
+// // };
+
+// // const rejectionHandler = (
+// //   state: UserInitialState,
+// //   action: PayloadAction<{ message: string } | undefined>,
+// // ) => {
+// //   state.isLoading = false;
+// //   state.error = action.payload?.message ?? "Unexpected error occurred";
+// // };
+
+// const userSlice = createSlice({
+//   name: "user",
+//   initialState,
+//   reducers: {
+//     logOut: (state) => {
+//       state.user = { name: null, email: null };
+//       state.isLoggedIn = false;
+//       state.isRefreshing = false;
+//       state.error = null;
+//     },
+//   },
+//   extraReducers(builder) {
+//     builder
+//       // .addCase(signup.pending, pendingHandler)
+//       // .addCase(
+//       //   signup.fulfilled,
+//       //   (
+//       //     state: UserInitialState,
+//       //     action: PayloadAction<{ name: string; email: string }>
+//       //   ) => {
+//       //     state.isLoading = false;
+//       //     state.isLoggedIn = true;
+//       //     state.user = action.payload;
+//       //   }
+//       // )
+//       // .addCase(signup.rejected, rejectionHandler)
+
+//       // .addCase(signin.pending, pendingHandler)
+//       // .addCase(
+//       //   signin.fulfilled,
+//       //   (
+//       //     state: UserInitialState,
+//       //     action: PayloadAction<{ name: string; email: string }>
+//       //   ) => {
+//       //     state.isLoading = false;
+//       //     state.isLoggedIn = true;
+//       //     state.user = action.payload;
+//       //   }
+//       // )
+//       // .addCase(signin.rejected, rejectionHandler)
+
+//       // .addCase(signout.pending, pendingHandler)
+//       // .addCase(signout.fulfilled, (state: UserInitialState) => {
+//       //   state.user = initialState.user;
+//       //   state.isLoading = initialState.isLoading;
+//       //   state.isLoggedIn = initialState.isLoggedIn;
+//       // })
+//       // .addCase(signout.rejected, rejectionHandler)
+
+//       .addCase(getMe.pending, (state: UserInitialState) => {
+//         state.isRefreshing = true;
+//       })
+//       .addCase(
+//         getMe.fulfilled,
+//         (
+//           state: UserInitialState,
+//           action: PayloadAction<{ name: string; email: string }>,
+//         ) => {
+//           // console.log(action.payload);
+//           state.user = action.payload;
+//           state.isRefreshing = false;
+//           state.isLoggedIn = true;
+//         },
+//       )
+//       .addCase(
+//         getMe.rejected,
+//         (
+//           state: UserInitialState,
+//           action: PayloadAction<{ message: string } | undefined>,
+//         ) => {
+//           state.isRefreshing = false;
+//           state.error = action.payload?.message ?? "Unexpected error occurred";
+//         },
+//       );
+//   },
+// });
+
+// export const { logOut } = userSlice.actions;
+// export const userReducer = userSlice.reducer;
+
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { getMe } from "./userRequests";
+
+interface UserInitialState {
+  user: {
+    name: string | null;
+    email: string | null;
+  };
+  token: string | null;
+  isLoading: boolean;
+  isLoggedIn: boolean;
+  error: string | null;
+  isRefreshing: boolean;
+}
+
 const initialState: UserInitialState = {
   user: {
     name: null,
     email: null,
   },
+  token: null,
   isLoading: false,
   isLoggedIn: false,
   error: null,
   isRefreshing: true,
 };
-
-// const pendingHandler = (state: UserInitialState) => {
-//   state.error = null;
-//   state.isLoading = true;
-// };
-
-// const rejectionHandler = (
-//   state: UserInitialState,
-//   action: PayloadAction<{ message: string } | undefined>,
-// ) => {
-//   state.isLoading = false;
-//   state.error = action.payload?.message ?? "Unexpected error occurred";
-// };
 
 const userSlice = createSlice({
   name: "user",
@@ -53,6 +160,7 @@ const userSlice = createSlice({
   reducers: {
     logOut: (state) => {
       state.user = { name: null, email: null };
+      state.token = null;
       state.isLoggedIn = false;
       state.isRefreshing = false;
       state.error = null;
@@ -60,42 +168,6 @@ const userSlice = createSlice({
   },
   extraReducers(builder) {
     builder
-      // .addCase(signup.pending, pendingHandler)
-      // .addCase(
-      //   signup.fulfilled,
-      //   (
-      //     state: UserInitialState,
-      //     action: PayloadAction<{ name: string; email: string }>
-      //   ) => {
-      //     state.isLoading = false;
-      //     state.isLoggedIn = true;
-      //     state.user = action.payload;
-      //   }
-      // )
-      // .addCase(signup.rejected, rejectionHandler)
-
-      // .addCase(signin.pending, pendingHandler)
-      // .addCase(
-      //   signin.fulfilled,
-      //   (
-      //     state: UserInitialState,
-      //     action: PayloadAction<{ name: string; email: string }>
-      //   ) => {
-      //     state.isLoading = false;
-      //     state.isLoggedIn = true;
-      //     state.user = action.payload;
-      //   }
-      // )
-      // .addCase(signin.rejected, rejectionHandler)
-
-      // .addCase(signout.pending, pendingHandler)
-      // .addCase(signout.fulfilled, (state: UserInitialState) => {
-      //   state.user = initialState.user;
-      //   state.isLoading = initialState.isLoading;
-      //   state.isLoggedIn = initialState.isLoggedIn;
-      // })
-      // .addCase(signout.rejected, rejectionHandler)
-
       .addCase(getMe.pending, (state: UserInitialState) => {
         state.isRefreshing = true;
       })
@@ -105,7 +177,6 @@ const userSlice = createSlice({
           state: UserInitialState,
           action: PayloadAction<{ name: string; email: string }>,
         ) => {
-          // console.log(action.payload);
           state.user = action.payload;
           state.isRefreshing = false;
           state.isLoggedIn = true;
@@ -117,6 +188,7 @@ const userSlice = createSlice({
           state: UserInitialState,
           action: PayloadAction<{ message: string } | undefined>,
         ) => {
+          state.token = null;
           state.isRefreshing = false;
           state.error = action.payload?.message ?? "Unexpected error occurred";
         },
