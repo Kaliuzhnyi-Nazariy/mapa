675a41d52c075a27afc4c658be9caa029174125d
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-09-22 10:13:05 +0200

    fix: try

--- frontend/src/redux/api.config.ts
+++ frontend/src/redux/api.config.ts
@@ -1,19 +1,19 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.PROD
-//     ? "https://mapa-app.onrender.com/api"
-//     : "http://localhost:3001/api",
-//   withCredentials: true,
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD ? "/api" : "http://localhost:3001/api",
+  baseURL: import.meta.env.PROD
+    ? "https://mapa-app.onrender.com/api"
+    : "http://localhost:3001/api",
   withCredentials: true,
 });
 
 export default api;
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.PROD ? "/api" : "http://localhost:3001/api",
+//   withCredentials: true,
+// });
+
+// export default api;
