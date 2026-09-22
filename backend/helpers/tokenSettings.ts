aaa6015a1e369b84ab5b3190a55eaa1942580c4b
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-09-22 10:21:06 +0200

    fix: change sameSite

--- backend/helpers/tokenSettings.ts
+++ backend/helpers/tokenSettings.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from "express";
 export const tokenSettings: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  sameSite: "none",
   path: "/",
   maxAge: 86400000,
 };
