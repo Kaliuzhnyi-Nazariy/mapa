15548a7c2762a180072c8260e4140d711c0b27f0
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-09-22 09:53:47 +0200

    fix: changed setting of cookies logci

--- backend/helpers/tokenSettings.ts
+++ backend/helpers/tokenSettings.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from "express";
 export const tokenSettings: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  sameSite: "lax",
   path: "/",
   maxAge: 86400000,
 };
