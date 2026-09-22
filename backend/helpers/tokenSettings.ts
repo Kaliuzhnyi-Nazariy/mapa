f496a3d7f8959254e56a45a46d48c28891cdae39
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-09-22 22:51:55 +0200

    feat: change token settings

--- backend/helpers/tokenSettings.ts
+++ backend/helpers/tokenSettings.ts
@@ -1,9 +1,17 @@
 import { CookieOptions } from "express";
 
+// export const tokenSettings: CookieOptions = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production",
+//   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+//   path: "/",
+//   maxAge: 86400000,
+// };
+
 export const tokenSettings: CookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true, // Wymuś true (Render i tak działa na HTTPS)
+  sameSite: "none", // Wymuś "none" dla cross-domain
   path: "/",
   maxAge: 86400000,
 };
